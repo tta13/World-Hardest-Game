eaaa88c8e373e5ce3b4ef7ba9b05fa9c73ab3ee7
Tales Tomaz Alves <tta@cin.ufpe.br>
2020-02-11 15:13:38 -0300

    Adicionando Inimigos

---
+++ World Hardest Game/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] enemies;
+    Vector2 initialPlayerPosition = new Vector2(-6.0f, -2.0f);
+    Vector2 initialEvenEnemyPosition = new Vector2(-3.5f, 2.0f);
+    Vector2 initialOddEnemyPosition = new Vector2(3.5f, 1.0f);
+    float playerSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.transform.position = initialPlayerPosition;
+        for(int i = 0; i<enemies.Length; i++)
+        {
+            if(i%2 == 0)
+            {
+                enemies[i].transform.position = initialEvenEnemyPosition;
+                initialEvenEnemyPosition -= new Vector2(0.0f, 2.0f);
+            }
+            else
+            {
+                enemies[i].transform.position = initialOddEnemyPosition;
+                initialOddEnemyPosition -= new Vector2(0.0f, 2.0f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += Vector3.up * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on W");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player. transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on A");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += Vector3.down * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on S");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on D");
+        }
+    }
+}
