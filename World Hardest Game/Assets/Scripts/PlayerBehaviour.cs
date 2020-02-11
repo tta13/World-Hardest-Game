410621d47d797407398759f4b26307913edd02b0
Tales Tomaz Alves <tta@cin.ufpe.br>
2020-02-11 13:34:57 -0300

    Adicionando Movimentação e Colisão ao Player

--- World Hardest Game/Assets/Scripts/PlayerBehaviour.cs
+++ World Hardest Game/Assets/Scripts/PlayerBehaviour.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     Vector2 initialPosition = new Vector2(-6.0f, -2.0f);
+    float playerSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = initialPosition;  
-
+        transform.position = initialPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on W");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on A");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on S");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+            //Debug.Log("You clicked on D");
+        }
     }
 }
