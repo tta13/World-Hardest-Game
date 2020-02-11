9c5d77cc4fb3f3c08b9e0f408c812c08c90f04a0
Tales Tomaz Alves <tta@cin.ufpe.br>
2020-02-11 13:13:22 -0300

    Adicionando Player e Primeiro Script

---
+++ World Hardest Game/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    Vector2 initialPosition = new Vector2(-6.0f, -2.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = initialPosition;  
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
