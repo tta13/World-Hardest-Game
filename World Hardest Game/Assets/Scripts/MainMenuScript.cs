d7c6c24d83f06f1e97bb1c64e3d3be8ac9fa8af9
Tales Tomaz Alves <tta@cin.ufpe.br>
2020-02-11 22:55:47 -0300

    Adicionando Menu Simples

---
+++ World Hardest Game/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
