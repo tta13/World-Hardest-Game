f7a013088da009071e9ef76ab4ef8f2e989ad510
Tales Tomaz Alves <tta@cin.ufpe.br>
2020-02-17 13:37:27 -0300

    Adicionando transição

--- World Hardest Game/Assets/Scripts/MainMenuScript.cs
+++ World Hardest Game/Assets/Scripts/MainMenuScript.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
     public void PlayGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1));
     }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelIndex);
+    }
+
 }
