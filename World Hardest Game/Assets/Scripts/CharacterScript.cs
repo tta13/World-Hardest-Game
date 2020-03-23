2a980e0d4d41a495c1d06925ee5414c309b9eff4
tta13 <tta@cin.ufpe.br>
2020-03-23 14:27:21 -0300

    Creating abstract class to deal with different characters

---
+++ World Hardest Game/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterScript : MonoBehaviour
+{
+    [SerializeField]
+    private float x, y;
+
+    void Start()
+    {
+        SetInitialPos();
+    }
+
+    public virtual void SetInitialPos()
+    {
+        gameObject.transform.position = new Vector2(x, y);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public abstract void Move();
+}
